75700adab94f50ec97c386ffeebce1d263e8d730
201902482dh <101381576+201902482dh@users.noreply.github.com>
2022-06-07 21:37:35 +0900

    gameover.png>jpg

--- 2048_main.py
+++ 2048_main.py
@@ -209,7 +209,7 @@ def display_rank_screen(): # 랭킹화면
 def display_game_over():
   pygame.display.set_caption("GAME OVER")
   global click_button 
-  background = pygame.image.load("game_over.png") # 게임오버 배경이미지 로드
+  background = pygame.image.load("game_over.jpg") # 게임오버 배경이미지 로드
   screen.blit(background, (0,0)) # 게임오버 배경 이미지 적용
   score_font = pygame.font.Font(None, 40)
   score = score_font.render(str(map.score), True, (0,0,0))
